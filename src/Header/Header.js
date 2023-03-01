212167ddf7b66a1b2d07619e00b554607cb4c4b8
mohamedfazilk <mohamedfazil961@gmail.com>
2023-03-01 22:36:18 +0530

    add the logo for the header

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header-container'>
+        
+        Header
+        <div className="header-left">
+            <img className='header_logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Zomato_Logo.svg/2560px-Zomato_Logo.svg.png" alt="" />
+
+        </div>
+
+        <div className="header-right">
+
+        </div>
+        </div>
+  )
+}
+
+export default Header
