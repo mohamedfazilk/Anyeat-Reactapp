802e8fa437ad11ee1641335d5c606d7cceda7cad
mohamedfazilk <mohamedfazil961@gmail.com>
2023-03-05 23:03:10 +0530

    place the login and sign up container

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -11,22 +11,23 @@ import InputBase from '@mui/material/InputBase';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import { TextField } from '@mui/material';
 import InputAdornment from "@mui/material/InputAdornment";
+import { withStyles } from '@mui/material/styles';
+import { useRef, useState } from 'react';
+import { IconButton } from '@mui/material';
 
 
 const Header = () => {
 
 
-
-
   return (
     <div className='header-container'>
 
-      Header
       <div className="header-left">
         <img className='header_logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Zomato_Logo.svg/2560px-Zomato_Logo.svg.png" alt="" />
         <div className="header_searchpart">
           <div className="drop_icon">
             <form className='searchinputdrop'>
+
               <TextField id="my-dropdown"
                 select
                 variant="standard"
@@ -34,11 +35,17 @@ const Header = () => {
                   disableUnderline: true,
                   startAdornment: (
                     <InputAdornment>
-                      < FmdGoodIcon />
+                      < FmdGoodIcon className='map' />
+                      <span>select your  location</span>
                     </InputAdornment>
                   )
+                }}
 
-                }}>
+
+              >
+                <MenuItem value="" disabled>
+                  <em>Select an option</em>
+                </MenuItem>
                 <MenuItem value="option1">Malappuram</MenuItem>
                 <MenuItem value="option2">Option 2</MenuItem>
                 <MenuItem value="option3">Option 3</MenuItem>
@@ -60,13 +67,14 @@ const Header = () => {
               />
             </form>
           </div>
-
-
-
         </div>
       </div>
 
       <div className="header-right">
+        <div className="header_auth">
+          <span className='header_optionOne'>Login</span>
+          <span className='header_optionTwo'>Sign Up</span>
+        </div>
 
       </div>
     </div>
